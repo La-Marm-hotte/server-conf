1f6b03b5d30a7ef41a2fa79eab1b654b76893fc5
Sylvain <sylvain@sbx.pm>
2021-11-21 22:17:58 +0100

    add password for auto unlocking

--- root/bwAutoConfirm.sh
+++ root/bwAutoConfirm.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 bw() {
-  docker run --rm -it -v /root/.config:/root/.config --user "$UID" lamarmhotte/bitwarden-cli:1.19.1 "$@"
+  docker run --rm -v /root/.config:/root/.config --user "$UID" lamarmhotte/bitwarden-cli:1.19.1 "$@"
 }
 
 jq() {
-  docker run --rm -i -v "${PWD}:/data" imega/jq "$@"
+  docker run --rm -v "${PWD}:/data" imega/jq "$@"
 }
 
-organization_id="example-org"
-session_key="$(bw unlock --raw)"
+organization_id="example-org-guid"
+master_pwd="xxxxxxxxxxxxxxx"
+
+session_key="$(bw unlock "$master_pwd" --raw)"
 org_members="$(bw list --session $session_key org-members --organizationid $organization_id | jq -c '.[] | select( .status == 1 )' | jq -c '.id' | tr -d '"')"
 for member_id in ${org_members[@]} ; do
         bw confirm --session $session_key org-member $member_id --organizationid $organization_id
