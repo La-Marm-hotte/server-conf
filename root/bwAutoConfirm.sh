b41f137a01ce292ef5f12c610d654b241b4777b8
Sylvain <sylvain@sbx.pm>
2021-11-22 21:59:37 +0100

    fix and comment script

--- root/bwAutoConfirm.sh
+++ root/bwAutoConfirm.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# This script will auto-confirm organization members, synced by of the bitwarden directory connector, when they
+# were connected at least once and after having created their password
+
+# It is intented to be run periodically by a systemctl timer
+
 bw() {
   docker run --rm -v /root/.config:/root/.config --user "$UID" lamarmhotte/bitwarden-cli:1.19.1 "$@"
 }
 
 jq() {
-  docker run --rm -v "${PWD}:/data" imega/jq "$@"
+  docker run --rm -v "${PWD}:/data" -i imega/jq "$@"
 }
 
-organization_id="example-org-guid"
+organization_id="xxxxxorg-guid-xxxx-xxxx-xxxxxxxxxxxx"
 master_pwd="xxxxxxxxxxxxxxx"
 
 session_key="$(bw unlock "$master_pwd" --raw)"
